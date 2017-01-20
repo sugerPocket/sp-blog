fdc3ace895a12907ea85b4afc7620628d808a507
邓廷礼 <937169359@qq.com>
2017-01-21 00:41:12 +0800

    save works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(req.url, res, next);
+  res.sendfile('/index.html', { root: __dirname.substr(0, __dirname.lastIndexOf('/')) + '/views'});
+});
+
+router.get('/includes', function (req, res) {
+  let name = req.params.name;
+  res.render('includes/' + name);
+});
+
+router.get('/partials', function (req, res) {
+  let name = req.params.name;
+  res.render('partials/' + name);
 });
 
 module.exports = router;
