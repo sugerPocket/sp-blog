c55e5539ddae0a5ca015b7e547695fdee8bff662
邓廷礼 <937169359@qq.com>
2017-03-01 15:05:24 +0800

    save works

--- blog-fe/assignment/new/controller.js
+++ blog-fe/assignment/new/controller.js
@@ -14,7 +14,23 @@
     activate();
 
     ///////////////工具函数
+    function createOneAssignment() {
+      let title = vm.title;
+      let content = vm.assignmentContent;
+      let type = vm.assignmentType;
+      let week = vm.week;
+      let fileEntry = angular.extend({}, vm.fileEntry);
 
+      let data = {
+        title,
+        content,
+        type,
+        week,
+        fileEntry
+      };
+
+      
+    }
     //////////////事件处理
 
     //////////////资源
@@ -23,12 +39,19 @@
     //////////////初始化变量
     function initVariable() {
       vm.title = '';
+      vm.assignmentContent = '';
 
       let assignmentTypes = vm.assignmentTypes = ['Computer Organization', 'Database', 'Operating System'];
       vm.assignmentType = assignmentTypes[0];
       
-      let weeks = vm.weeks = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen', 'Twenty', 'Twenty One'];
+      let weeks = vm.weeks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21'];
       vm.week = weeks[0];
+
+      vm.fileEntry = {
+        allowable: false,
+        maxSize: 0,
+        nameRegExp: ''
+      };
     }
 
     //////////////使用参数初始化变量
